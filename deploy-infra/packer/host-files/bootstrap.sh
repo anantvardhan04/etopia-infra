5749d6a4623d2f4e83d2f9ac765dfb18455dcd3b
Anant Vardhan <anant_vardhan@vfc.com>
2023-02-17 19:58:00 +0530

    Added deploy artifacts

--- deploy-infra/packer/host-files/bootstrap.sh
+++ deploy-infra/packer/host-files/bootstrap.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Install prerequisites
-sudo yum -y -q update
-sudo amazon-linux-extras install -y nginx1
+yum -y -q update
+amazon-linux-extras install -y nginx1
 
 # Configure nginx
-sudo cp -f /tmp/host-files/nginx.conf /etc/nginx/nginx.conf
-sudo systemctl enable nginx && sudo systemctl start nginx
+cp -f /tmp/host-files/nginx.conf /etc/nginx/nginx.conf
+systemctl enable nginx && sudo systemctl start nginx
 
 # Create Slackapp webhook service
-sudo python3 -m venv /opt/slackapp/webhook
-sudo /opt/slackapp/webhook/bin/pip install --upgrade pip
-sudo cp /tmp/host-files/slackapp/app.py /tmp/host-files/slackapp/requirements.txt /opt/slackapp/webhook/
-sudo cp /tmp/host-files/slackapp/app.service /usr/lib/systemd/system/slackapp.service
+python3 -m venv /opt/slackapp/webhook
+/opt/slackapp/webhook/bin/pip install --upgrade pip
+cp /tmp/host-files/slackapp/app.py /tmp/host-files/slackapp/requirements.txt /opt/slackapp/webhook/
+cp /tmp/host-files/slackapp/app.service /usr/lib/systemd/system/slackapp.service
 pip3 install --user -r /opt/slackapp/webhook/requirements.txt
-sudo systemctl enable slackapp.service && sudo systemctl start slackapp.service
+systemctl enable slackapp.service && sudo systemctl start slackapp.service

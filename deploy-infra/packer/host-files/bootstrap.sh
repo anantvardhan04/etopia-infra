946cfa82ea4b0f14ec16aeb5f9c07db90eef17f3
Anant Vardhan <anant_vardhan@vfc.com>
2023-02-23 02:18:45 +0530

    Added Datadog changes

--- deploy-infra/packer/host-files/bootstrap.sh
+++ deploy-infra/packer/host-files/bootstrap.sh
@@ -8,6 +8,17 @@ sudo amazon-linux-extras install -y nginx1
 sudo cp -f /tmp/host-files/nginx.conf /etc/nginx/nginx.conf
 sudo systemctl enable nginx && sudo systemctl start nginx
 
+# Configure Datadog
+
+datadog_agent_version=7.33.1-1
+wget -nv https://s3.amazonaws.com/yum.datadoghq.com/stable/7/x86_64/datadog-agent-${datadog_agent_version}.x86_64.rpm
+sudo yum localinstall -y datadog-agent-${datadog_agent_version}.x86_64.rpm
+rm datadog-agent-${datadog_agent_version}.x86_64.rpm
+sudo -udd-agent cp -vR datadog-agent /etc/
+sudo sed -i "s/# tags:.*/tags: \n  - app: fonetwish/" /etc/datadog-agent/datadog.yaml
+sudo sed -i "s/# env: <environment name>/env: dev/" /etc/datadog-agent/datadog.yaml
+
+
 # Create Slackapp webhook service
 sudo python3 -m venv /opt/slackapp/webhook
 sudo /opt/slackapp/webhook/bin/pip install --upgrade pip
